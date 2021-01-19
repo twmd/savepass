f233921227248ae4efc4d223264bab04f1e97b42
twmd <slava.sazanov@gmail.com>
2021-01-19 17:11:55 +0300

    Version 1. add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='savepass',
+      version='1.0',
+      description='example save pass in keychain',
+      author='Vyacheslav Sazanov',
+      author_email='slava.sazanov@gmail.com',
+      packages=['savepass'],
+      install_requires=['paramiko', 'keyring', 'getpass']
+      )
